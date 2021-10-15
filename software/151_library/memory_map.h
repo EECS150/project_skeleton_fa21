2264343c28ac885440c33e26a7b9dfb4e27f9697
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2021-10-15 15:11:38 -0700

    all software

--- software/151_library/memory_map.h
+++ software/151_library/memory_map.h
@@ -1,5 +1,9 @@
 #include "types.h"
 
+#define csr_tohost(csr_val) { \
+    asm volatile ("csrw 0x51e,%[v]" :: [v]"r"(csr_val)); \
+}
+
 #define COUNTER_RST (*((volatile uint32_t*) 0x80000018))
 #define CYCLE_COUNTER (*((volatile uint32_t*)0x80000010))
 #define INSTRUCTION_COUNTER (*((volatile uint32_t*)0x80000014))
---
+++ software/mmult/benchmark.h
@@ -0,0 +1,7 @@
+#ifndef BENCHMARK_H_
+#define BENCHMARK_H_
+
+#include "types.h"
+
+void run_and_time(uint32_t (*f)());
+#endif
